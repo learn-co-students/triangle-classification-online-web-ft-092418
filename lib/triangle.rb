da17c1fbf209dee3860bc133cbe0403a454acc51
lafesh <ginafedder@yahoo.com>
2018-10-12 09:52:49 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,25 +10,22 @@ class Triangle
   end
 
   def kind
-    error1 = @a = 0 || @b = 0 || @c = 0  
-    error2 = @a + @b < @c || @b + @c < @a || @a + @c < @b 
+    error1 = @a == 0 || @b == 0 || @c == 0  
+    error2 = @a + @b <= @c || @b + @c <= @a || @a + @c <= @b 
     error3 = @a.negative? || @b.negative? || @c.negative?
 
     if error1 || error2 || error3
       begin
         raise TriangleError
-      end 
-    end 
-    if @a == @b && @b == @c
+      end  
+    elsif @a == @b && @b == @c
       :equilateral
-    end 
-    if @a == @b || @a == @c || @b == @c
+    elsif @a == @b || @a == @c || @b == @c
       :isosceles 
-    end 
-    if @a != @b && @b != @c 
-      :scalene
-    #else 
+    #elsif @a != @b && @b != @c 
       #:scalene
+    else 
+      :scalene
     end     
   end 
 
