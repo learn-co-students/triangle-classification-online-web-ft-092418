7c2e290f624f67ba52440e3f53856c0fc7a35cb7
lafesh <ginafedder@yahoo.com>
2018-10-12 09:40:54 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,34 @@
-require 'rspec'
+
 class Triangle
-  attr_accessor :lengthab, :lengthbc, :lengthca
 
-  def kind 
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
 
+  def kind
+    error1 = @a = 0 || @b = 0 || @c = 0  
+    error2 = @a + @b < @c || @b + @c < @a || @a + @c < @b 
+    error3 = @a.negative? || @b.negative? || @c.negative?
+
+    if error1 || error2 || error3
+      begin
+        raise TriangleError
+      end 
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles 
+    #elsif @a != @b && @b != @c 
+      #:scalene
+    else 
+      :scalene
+    end     
   end 
 
   class TriangleError < StandardError
-
   end 
 end
